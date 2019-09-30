63e0cc90bb33ebfb11e6e4b3d7df00da9318d273
gdepuydt <geert_depuydt@hotmail.com>
2019-09-30 22:36:27 +0200

    start of a very long journey

--- lib/src/window.rs
+++ lib/src/window.rs
@@ -8,6 +8,181 @@ use std::iter::once;
 use std::io::Error;
 use std::ptr::null_mut;
 
+#[repr(C)] 
+#[derive(Copy)]
+#[cfg_attr(feature = "impl-debug", derive(Debug))]
+struct GUID {
+    Data1: c_ulong,
+    Data2: c_ushort,
+    Data3: c_ushort,
+    Data4: [c_uchar; 8],
+}
+
+impl Clone for GUID {
+    #[inline]
+    fn clone(&self) -> GUID { *self }
+}
+        
+#[cfg(feature = "impl-default")]
+impl Default for GUID {
+    #[inline]
+    fn default() -> GUID { unsafe { _core::mem::zeroed() } }
+}
+
+
+// This is the huge macro called for implementing IUnknown interface, which I need... I will need a tool to deconstruct this monster
+// C:\dev\winapi-rs\src\macros.rs
+
+#[macro_export]
+macro_rules! RIDL {
+    (#[uuid($l:expr, $w1:expr, $w2:expr,
+        $b1:expr, $b2:expr, $b3:expr, $b4:expr, $b5:expr, $b6:expr, $b7:expr, $b8:expr)]
+    class $class:ident;) => (
+        pub enum $class {}
+        impl $crate::Class for $class {
+            #[inline]
+            fn uuidof() -> $crate::shared::guiddef::GUID {
+                $crate::shared::guiddef::GUID {
+                    Data1: $l,
+                    Data2: $w1,
+                    Data3: $w2,
+                    Data4: [$b1, $b2, $b3, $b4, $b5, $b6, $b7, $b8],
+                }
+            }
+        }
+    );
+    (#[uuid($($uuid:expr),+)]
+    interface $interface:ident ($vtbl:ident) {$(
+        $(#[$($attrs:tt)*])* fn $method:ident($($p:ident : $t:ty,)*) -> $rtr:ty,
+    )+}) => (
+        RIDL!{@vtbl $interface $vtbl () $(
+            $(#[$($attrs)*])* fn $method($($p: $t,)*) -> $rtr,
+        )+}
+        #[repr(C)]
+        pub struct $interface {
+            pub lpVtbl: *const $vtbl,
+        }
+        impl $interface {
+            $(RIDL!{@method $(#[$($attrs)*])* fn $method($($p: $t,)*) -> $rtr})+
+        }
+        RIDL!{@uuid $interface $($uuid),+}
+    );
+    (#[uuid($($uuid:expr),+)]
+    interface $interface:ident ($vtbl:ident) : $pinterface:ident ($pvtbl:ident) {}) => (
+        RIDL!{@vtbl $interface $vtbl (pub parent: $pvtbl,)}
+        #[repr(C)]
+        pub struct $interface {
+            pub lpVtbl: *const $vtbl,
+        }
+        RIDL!{@deref $interface $pinterface}
+        RIDL!{@uuid $interface $($uuid),+}
+    );
+    (#[uuid($($uuid:expr),+)]
+    interface $interface:ident ($vtbl:ident) : $pinterface:ident ($pvtbl:ident) {$(
+        $(#[$($attrs:tt)*])* fn $method:ident($($p:ident : $t:ty,)*) -> $rtr:ty,
+    )+}) => (
+        RIDL!{@vtbl $interface $vtbl (pub parent: $pvtbl,) $(
+            $(#[$($attrs)*])* fn $method($($p: $t,)*) -> $rtr,
+        )+}
+        #[repr(C)]
+        pub struct $interface {
+            pub lpVtbl: *const $vtbl,
+        }
+        impl $interface {
+            $(RIDL!{@method $(#[$($attrs)*])* fn $method($($p: $t,)*) -> $rtr})+
+        }
+        RIDL!{@deref $interface $pinterface}
+        RIDL!{@uuid $interface $($uuid),+}
+    );
+    (@deref $interface:ident $pinterface:ident) => (
+        impl $crate::_core::ops::Deref for $interface {
+            type Target = $pinterface;
+            #[inline]
+            fn deref(&self) -> &$pinterface {
+                unsafe { &*(self as *const $interface as *const $pinterface) }
+            }
+        }
+    );
+    (@method fn $method:ident($($p:ident : $t:ty,)*) -> $rtr:ty) => (
+        #[inline] pub unsafe fn $method(&self, $($p: $t,)*) -> $rtr {
+            ((*self.lpVtbl).$method)(self as *const _ as *mut _, $($p,)*)
+        }
+    );
+    (@method #[fixme] fn $method:ident($($p:ident : $t:ty,)*) -> $rtr:ty) => (
+        #[inline] pub unsafe fn $method(&self, $($p: $t,)*) -> $rtr {
+            let mut ret = $crate::_core::mem::uninitialized();
+            ((*self.lpVtbl).$method)(self as *const _ as *mut _, &mut ret, $($p,)*);
+            ret
+        }
+    );
+    (@vtbl $interface:ident $vtbl:ident ($($fields:tt)*)
+        $(fn $method:ident($($p:ident : $t:ty,)*) -> $rtr:ty,)*
+    ) => (
+        RIDL!{@item #[repr(C)]
+        pub struct $vtbl {
+            $($fields)*
+            $(pub $method: unsafe extern "system" fn(
+                This: *mut $interface,
+                $($p: $t,)*
+            ) -> $rtr,)*
+        }}
+    );
+    (@vtbl $interface:ident $vtbl:ident ($($fields:tt)*)
+        fn $method:ident($($p:ident : $t:ty,)*) -> $rtr:ty,
+    $($tail:tt)*) => (
+        RIDL!{@vtbl $interface $vtbl (
+            $($fields)*
+            pub $method: unsafe extern "system" fn(
+                This: *mut $interface,
+                $($p: $t,)*
+            ) -> $rtr,
+        ) $($tail)*}
+    );
+    (@vtbl $interface:ident $vtbl:ident ($($fields:tt)*)
+        #[fixme] fn $method:ident($($p:ident : $t:ty,)*) -> $rtr:ty,
+    $($tail:tt)*) => (
+        RIDL!{@vtbl $interface $vtbl (
+            $($fields)*
+            pub $method: unsafe extern "system" fn(
+                This: *mut $interface,
+                ret: *mut $rtr,
+                $($p: $t,)*
+            ) -> *mut $rtr,
+        ) $($tail)*}
+    );
+    (@uuid $interface:ident
+        $l:expr, $w1:expr, $w2:expr,
+        $b1:expr, $b2:expr, $b3:expr, $b4:expr, $b5:expr, $b6:expr, $b7:expr, $b8:expr
+    ) => (
+        impl $crate::Interface for $interface {
+            #[inline]
+            fn uuidof() -> $crate::shared::guiddef::GUID {
+                $crate::shared::guiddef::GUID {
+                    Data1: $l,
+                    Data2: $w1,
+                    Data3: $w2,
+                    Data4: [$b1, $b2, $b3, $b4, $b5, $b6, $b7, $b8],
+                }
+            }
+        }
+    );
+    (@item $thing:item) => ($thing);
+}
+
+// This is the macro call for implementing the IUnknown interface...
+// C:\dev\winapi-rs\src\um\unknwnbase.rs
+RIDL!{#[uuid(0x00000000, 0x0000, 0x0000, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46)]
+interface IUnknown(IUnknownVtbl) {
+    fn QueryInterface(
+        riid: REFIID,
+        ppvObject: *mut *mut c_void,
+    ) -> HRESULT,
+    fn AddRef() -> ULONG,
+    fn Release() -> ULONG,
+}}
+pub type LPUNKNOWN = *mut IUnknown;
+
+type c_uchar = u8;
 type c_ushort = u16;
 type c_uint = u64;
 type c_ulong = u64;
@@ -17,6 +192,7 @@ type c_long = i32;
 type LONG_PTR = isize;
 type UINT_PTR = usize;
 type UINT = c_uint;
+type ULONG = c_ulong;
 type WORD = c_ushort;
 type DWORD = c_ulong;
 type ATOM = WORD;
@@ -29,6 +205,9 @@ type LPARAM = LONG_PTR;
 type WPARAM = UINT_PTR;
 type LRESULT = LONG_PTR;
 type LONG = c_long;
+type HRESULT = c_long;
+pub type IID = GUID;
+type REFIID = *const IID;
 
 const CS_OWNDC: UINT = 0x0020;
 const CS_HREDRAW: UINT = 0x0002;
@@ -105,6 +284,15 @@ extern "system" {
     pub fn DispatchMessageW(lpmsg: *const MSG) -> LRESULT;
     pub fn TranslateMessage(lpmsg: *const MSG) -> BOOL;
 }
+
+// from shcore.dll
+type GetDpiForSystem = unsafe extern "system" fn() -> UINT;
+type GetDpiForMonitor = unsafe extern "system" fn(HMONITOR, MONITOR_DPI_TYPE, *mut UINT, *mut UINT);
+
+//from user32.dll
+type SetProcessDpiAwareness = unsafe extern "system" fn(PROCESS_DPI_AWARENESS) ->HRESULT;
+type DCompositionCreateDevice2 = unsafe extern "system" fn(renderingDevice: *const IUnknown, iid: REFIID, dcompositionDevice: *mut *mut c_void,) -> HRESULT;
+type CreateDXGIFactory2 = unsafe extern "system" fn(Flags: UINT, riid: REFIID, ppFactory: *mut *mut c_void) -> HRESULT;
 
 #[repr(C)]
 pub struct WNDCLASSW {
